5a86eedf95745e2ca27a66e9ebf7d34fc6d658b7
Computer Lab 4 <cl4@uit.edu>
2026-05-22 11:12:39 +0500

    add

---
+++ Desktop/zai/task1.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Print a simple message to the console
+        System.out.println("Hello, World!");
+
+        // Example of variables and simple arithmetic
+        int number1 = 10;
+        int number2 = 20;
+        int sum = number1 + number2;
+
+        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
+    }
+}
