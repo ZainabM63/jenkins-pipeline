fbecfbbdaaaad5bc89ac4e2beb8987e11eeabd46
Zainab Mughal <zainabmughal33@gmail.com>
2026-05-22 11:19:04 +0500

    Update task1.java

--- task1.java
+++ task1.java
@@ -1,4 +1,4 @@
-public class HelloWorld {
+public class task1 {
     public static void main(String[] args) {
         // Print a simple message to the console
         System.out.println("Hello, World!");
